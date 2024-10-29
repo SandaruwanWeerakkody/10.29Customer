0027a86a2cba9d7cf01a0ba0b032ad488f54a137
Sandaruwan Weerakkody <154538944+SandaruwanWeerakkody@users.noreply.github.com>
2024-10-29 16:52:07 +0530

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,27 @@
+public class Customer{
+	private String name;
+	private boolean member = false;
+	private String memberType;
+	
+	public Customer(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+	public boolean isMember(){
+		return member;
+	}
+	public void setMember(boolean member){
+		this.member=member;
+	}
+	public String getMemberType(){
+		return memberType;
+	}
+	public void setMemberType(String type){
+		this.memberType=type;
+	}
+	public String toString(){
+		return "name: "+name+"member: "+member+"memberType: "+memberType;
+	}
+}
