2404c57d4c082dbff3a1de428b7a887a7bd34b78
Sandaruwan Weerakkody <154538944+SandaruwanWeerakkody@users.noreply.github.com>
2024-10-29 22:11:16 +0530

    Update DiscountRate.java

--- DiscountRate.java
+++ DiscountRate.java
@@ -1,35 +1,35 @@
 public class DiscountRate{
-	private static final double serviceDiscountPremium = 0.2;
-	private static final double serviceDiscountGold = 0.15;
-	private static final double serviceDiscountSilver = 0.1;
-	private static final double productDiscountPremium = 0.1;
-	private static final double productDiscountGold = 0.1;
-	private static final double productDiscountSilver = 0.1;
-	
-	
-	
-	public double getServiceDiscountRate(String type){
-		switch(type){
-			case "Premium":
-				return serviceDiscountPremium;
-			case "Gold":
-				return serviceDiscountGold;
-			case "Silver":
-				return serviceDiscountSilver;
-			default:
-				return 0;
-		}
-	}
-	public double getproductDiscountRate(String type){
-		switch(type){
-			case "Premium":
-				return productDiscountPremium;
-			case "Gold":
-				return productDiscountGold;
-			case "Silver":
-				return productDiscountSilver;
-			default:
-				return 0;
-		}
-	}
+    private static final double serviceDiscountPremium = 0.2;
+    private static final double serviceDiscountGold = 0.15;
+    private static final double serviceDiscountSilver = 0.1;
+    private static final double productDiscountPremium = 0.1;
+    private static final double productDiscountGold = 0.1;
+    private static final double productDiscountSilver = 0.1;
+
+
+
+    public static double getServiceDiscountRate(String type){
+        switch(type){
+            case "Premium":
+                return serviceDiscountPremium;
+            case "Gold":
+                return serviceDiscountGold;
+            case "Silver":
+                return serviceDiscountSilver;
+            default:
+                return 0;
+        }
+    }
+    public static double getProductDiscountRate(String type){
+        switch(type){
+            case "Premium":
+                return productDiscountPremium;
+            case "Gold":
+                return productDiscountGold;
+            case "Silver":
+                return productDiscountSilver;
+            default:
+                return 0;
+        }
+    }
 }
