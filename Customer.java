7fee18d2fb834034198632e5f8654aaf26c9024a
Sandaruwan Weerakkody <154538944+SandaruwanWeerakkody@users.noreply.github.com>
2024-10-29 22:11:53 +0530

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,27 +1,33 @@
 public class Customer{
-	private String name;
-	private boolean member = false;
-	private String memberType;
-	
-	public Customer(String name){
-		this.name = name;
-	}
-	public String getName(){
-		return name;
-	}
-	public boolean isMember(){
-		return member;
-	}
-	public void setMember(boolean member){
-		this.member=member;
-	}
-	public String getMemberType(){
-		return memberType;
-	}
-	public void setMemberType(String type){
-		this.memberType=type;
-	}
-	public String toString(){
-		return "name: "+name+"member: "+member+"memberType: "+memberType;
-	}
+    private String name;
+    private boolean member = false;
+    private String memberType;
+
+    public Customer(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public boolean isMember(){
+        return member;
+    }
+    public void setMember(boolean member){
+        this.member=member;
+    }
+
+    public String getMemberType(){
+        return memberType;
+    }
+    public void setMemberType(String type){
+        if(member){
+            this.memberType=type;
+        }else{
+            System.out.println("This is not a member");
+        }
+
+    }
+    public String toString(){
+        return "Name: " + name + ", Member: " + member + ", Member Type: " + memberType;
+    }
 }
